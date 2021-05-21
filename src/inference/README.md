05855424633bac71a19087c5ecce5a7497cda6d0
Shervyna Ruan <HsuanWei.Ruan@microsoft.com>
2021-05-21 17:26:34 +0900

    add logger, update readme for changing the start up command

--- src/inference/README.md
+++ src/inference/README.md
@@ -1,6 +1,6 @@
 # How to run the inference api
   1. Create .env file for azure storage credentials if needed. Please refer to template.env.
   2. Run the following command to start FastAPI:
-  ```
-  uvicorn main:app --reload
-  ```
+      ```
+      python main.py
+      ```
--- src/inference/main.py
+++ src/inference/main.py
@@ -1,27 +1,16 @@
 import os
+import json
+import logging
+import uvicorn
 from dotenv import load_dotenv
 from INCLUDE.evaluate import inference, get_inference_args
 from fastapi import FastAPI, HTTPException
 from azure.storage.blob import BlobServiceClient
 
 
-# Load environment variables
-load_dotenv()
-
-# Set up for azure blob service client if credentials are in env vars
-az_storage_connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-az_storage_container_name = os.environ.get("AZURE_STORAGE_CONTAINER_NAME")
-container_client = None
-if az_storage_connection_string and az_storage_container_name:
-    blob_service_client = BlobServiceClient.from_connection_string(az_storage_connection_string)
-    container_client = blob_service_client.get_container_client(az_storage_container_name)
-
-# Path to the downloaded videos from blob when reading from azure storage
-path_to_downloaded_blobs = "downloaded"
-if not os.path.exists(path_to_downloaded_blobs):
-    os.makedirs(path_to_downloaded_blobs)
-
+logger = logging.getLogger("uvicorn.error")
 app = FastAPI()
+container_client = None
 
 
 # You can specify whether to get the videos from blob storage or from local file system 
@@ -37,7 +26,7 @@ async def root(file_name: str, local_file_path:str = "", from_local:bool = False
         file_path = os.path.join(path_to_downloaded_blobs, file_name)
         # Saving blob to local file system
         with open(file_path, "wb") as blob:
-            blob.write(blob_client.download_blob().readall())
+            blob.write(blob_client.download_blob().readall())  
     else:
         raise HTTPException(status_code=400, detail="Missing required parameters.")
     
@@ -48,8 +37,27 @@ async def root(file_name: str, local_file_path:str = "", from_local:bool = False
             "file": file_name,
             "predicted_label": preds[0]["predicted_label"]
         }
+        logger.info("\nresult: \n" + json.dumps(response, indent=4))
         return response
+
     except AssertionError as error:
-        # print(error.args)
+        logger.error(error)
         raise HTTPException(status_code=400, detail=error.args[0])
 
+if __name__ == '__main__':
+    # Load environment variables
+    load_dotenv()
+
+    # Set up for azure blob service client if credentials are in env vars
+    az_storage_connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
+    az_storage_container_name = os.environ.get("AZURE_STORAGE_CONTAINER_NAME")
+    if az_storage_connection_string and az_storage_container_name:
+        blob_service_client = BlobServiceClient.from_connection_string(az_storage_connection_string)
+        container_client = blob_service_client.get_container_client(az_storage_container_name)
+
+        # Path to the downloaded videos from blob when reading from azure storage
+        path_to_downloaded_blobs = "downloaded"
+        if not os.path.exists(path_to_downloaded_blobs):
+            os.makedirs(path_to_downloaded_blobs)
+
+    uvicorn.run("main:app", reload=True)
